4eede8334a37374843da4e4b0b0b556a2051e8fb
annavaleriev <filenko.a@gmail.com>
2024-03-07 10:35:40 +0300

    Класс по работе с базой данных

--- DBManager.py
+++ DBManager.py
@@ -7,7 +7,7 @@ class DBManager:
     """
 
     # @staticmethod
-    def connect_database(self, sql):
+    def connection_database(self, sql):
         """
         Метод для подключения к базе данных
         :param sql:
@@ -30,25 +30,25 @@ class DBManager:
 
     def get_companies_and_vacancies_count(self):
         """ Метод, который получает список всех компаний и количество вакансий у каждой компании."""
-        return self.connect_database("""SELECT name_company, COUNT(name_vacancy) FROM company
+        return self.connection_database("""SELECT name_company, COUNT(name_vacancy) FROM company
                                      INNER JOIN vacancy ON company.id_company = vacancy.id_employer
                                      GROUP BY name_company""")
 
     def get_all_vacancies(self):
         """ Метод, который получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты
         и ссылки на вакансию."""
-        return self.connect_database("""SELECT name_company, name_vacancy, salary_from, salary_to, alternate_url
+        return self.connection_database("""SELECT name_company, name_vacancy, salary_from, salary_to, alternate_url
                                         FROM vacancy
                                         INNER JOIN company ON company.id_company = vacancy.id_employer""")
 
     def get_avg_salary(self):
         """" Метод, который получает среднюю зарплату по всем вакансиям."""
-        return self.connect_database("""SELECT AVG(salary_from) + AVG(salary_to) /2 AS avg_salary
+        return self.connection_database("""SELECT AVG(salary_from) + AVG(salary_to) /2 AS avg_salary
                                         FROM vacancy""")
 
     def get_vacancies_with_higher_salary(self):
         """Метод, который получает список всех вакансий, у которых зарплата выше средней по всем вакансиям."""
-        return self.connect_database("""SELECT name_vacancy
+        return self.connection_database("""SELECT name_vacancy
                                         FROM vacancy
                                         WHERE (salary_from + salary_to) / 2 >
                                         (SELECT (AVG(salary_from) + AVG(salary_to)) / 2 AS salary_avg 
@@ -56,7 +56,7 @@ class DBManager:
 
     def get_vacancies_with_keyword(self):
         """ Метод, который получает список всех вакансий, в названии которых содержатся переданные в метод слова"""
-        return self.connect_database("""SELECT name_vacancy
+        return self.connection_database("""SELECT name_vacancy
                                         FROM vacancy
                                         WHERE name_vacancy LIKE '%{word}%'""")
 
