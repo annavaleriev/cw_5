7b3724ad5f9a550d5e662a13594b0711d1135c4f
annavaleriev <filenko.a@gmail.com>
2024-03-11 21:07:28 +0300

    Создан class Service  и методы для запроса к базе данных

--- db_manager.py
+++ db_manager.py
@@ -1,7 +1,16 @@
+import configparser  # Импорт библиотеки configparser, это библиотека для работы с файлами конфигурации.
+
+from settings import DATABASE_CONFIG_PATH  # Импорт переменной DATABASE_CONFIG_PATH из файла settings.py
+
 import psycopg2  # Импорт библиотеки psycopg2, это библиотека для работы с базой данных PostgreSQL из Python.
 from psycopg2.extras import RealDictCursor  # Импорт библиотеки RealDictCursor - Этот тип курсора возвращает
+
 # результаты запроса в виде словаря, где ключами являются имена столбцов,
 # а значениями - соответствующие значения в строке результата.
+
+# Чтение кофигурационного файла
+config = configparser.ConfigParser()  # Создание объекта класса ConfigParser из библиотеки configparser
+config.read(DATABASE_CONFIG_PATH)  # Чтение файла конфигурации базы данных
 
 
 class DBManager:  # Создание контекстного менеджера внутри класса DBManager
@@ -14,12 +23,15 @@ class DBManager:  # Создание контекстного менеджера
     def __enter__(self):
         """Метод для подключения к базе данных"""
         try:  # Обработка исключений, если подключение не удалось
-            self.connection = psycopg2.connect(dbname='vacancy', user='postgres', password='30051980', host='localhost',
-                                               cursor_factory=RealDictCursor)  # Подключение к базе данных
-            #TODO актуальные данные подключения и спрятать. Или не тут прятать?
+            # self.connection = psycopg2.connect(dbname='vacancy', user='postgres', password='30051980', host='localhost',
+            #                                    cursor_factory=RealDictCursor)  # Подключение к базе данных
+            self.connection = psycopg2.connect(**config['database'], cursor_factory=RealDictCursor)
+            # Подключение к базе данных
+            # TODO актуальные данные подключения и спрятать. Или не тут прятать?
             self.cursor = self.connection.cursor()  # Создание курсора, который будет использоваться для выполнения запросов
         except psycopg2.OperationalError:  # Обработка исключений. OperationalError - это класс исключения, который
-            # предоставляется библиотекой psycopg2 для обработки ошибок, связанных с операциями, связанными с базой данных PostgreSQL.
+            # предоставляется библиотекой psycopg2 для обработки ошибок, связанных с операциями, связанными
+            # с базой данных PostgreSQL.
             raise ValueError("Не удалось подключиться к базе данных")  # Вывод ошибки, если подключение не удалось
         return self  # Возврат значения, если подключение удалось. self - это объект класса
 
@@ -28,44 +40,94 @@ class DBManager:  # Создание контекстного менеджера
         self.cursor.close()  # Закрытие курсора
         self.connection.close()  # Закрытие соединения
 
-    def get_all_vacancies(self):
-        """ Метод, который получает список всех вакансий с указанием названия компании,
-        названия вакансии и зарплаты и ссылки на вакансию."""
-        self.cursor.execute("""SELECT name_company, name_vacancy, salary_from, salary_to, alternate_url
-                                        FROM vacancy
-                                        INNER JOIN company ON company.id_company = vacancy.id_employer""")
-        # TODO: Проверить запрос,ошибка Unable to resolve column 'name_company'
-        # Выполнение запроса. Метод execute - это метод объекта курсора (cursor), который используется для выполнения
-        # SQL-запросов к базе данных. Он позволяет отправлять SQL-запросы на сервер базы данных PostgreSQL
-        # для выполнения и получения результата.
-        return self.cursor.fetchall()  # Возврат результата запроса, который был выполнен методом execute.
-        # Метод fetchall() является одним из методов объекта курсора в библиотеке psycopg2 и используется
-        # для извлечения всех строк результата выполнения запроса к базе данных.
+    # def get_all_vacancies(self):  # TODO: Перенести в класс Service? Там же все методы для работы с базой данных
+    #     """ Метод, который получает список всех вакансий с указанием названия компании,
+    #     названия вакансии и зарплаты и ссылки на вакансию."""
+    #     self.cursor.execute("""SELECT name_company, name_vacancy, salary_from, salary_to, alternate_url
+    #                                     FROM vacancy
+    #                                     INNER JOIN company ON company.id_company = vacancy.id_employer""")
+    #     # TODO: Проверить запрос,ошибка Unable to resolve column 'name_company'
+    #     # Выполнение запроса. Метод execute - это метод объекта курсора (cursor), который используется для выполнения
+    #     # SQL-запросов к базе данных. Он позволяет отправлять SQL-запросы на сервер базы данных PostgreSQL
+    #     # для выполнения и получения результата.
+    #     return self.cursor.fetchall()  # Возврат результата запроса, который был выполнен методом execute.
+    #     # Метод fetchall() является одним из методов объекта курсора в библиотеке psycopg2 и используется
+    #     # для извлечения всех строк результата выполнения запроса к базе данных.
 
 
 class Service:
-    __manager = None
+    """Класс для работы с базой данных"""
+    __manager = None  # Создание атрибута класса __manager, который будет использоваться для работы с базой данных
 
-    @property
+    @property  # Декоратор для создания свойства manager
     def manager(self):
-        if self.__manager is None:
-            raise NotImplementedError("afsdfjksdghfjk")
-        return self.__manager
+        """Метод для получения значения атрибута __manager"""
+        if self.__manager is None:  # Проверка, если атрибут __manager не установлен
+            raise NotImplementedError("""Менеджер базы данных не установлен. " 
+                                       Пожалуйста, установите менеджер перед выполнением операции.""")  # Вывод ошибки
+        return self.__manager  # Возврат значения атрибута __manager
 
-    @manager.setter
-    def manager(self, obj):
-        if not isinstance(obj, DBManager):
-            raise ValueError("sdfljkhsdjkfghsdkhfgsdkhjfg")
-        self.__manager = obj
+    @manager.setter  # Декоратор для установки значения атрибута __manager
+    def manager(self, object):
+        """Метод для установки значения атрибута __manager"""
+        if not isinstance(object, DBManager):  # Проверка, если объект не является экземпляром класса DBManager
+            raise ValueError("""Неверный тип объекта для установки атрибута manager. 
+                                Ожидается объект класса DBManager или его подкласса.""")  # Вывод ошибки
+        self.__manager = object  # Установка значения атрибута __manager
 
-    def get_vacancies(self):
-        with self.manager as manager:
-            manager.cursor.execute("dsfdsfdsfsdf")
-            return manager.cursor.fetchall()
+    def get_aLL_vacancies(self): # TODO: Проерить sql запрос и как-то протестировать
+        """ Метод, который получает список всех вакансий с указанием названия компании,
+        названия вакансии и зарплаты и ссылки на вакансию."""
+        with self.manager as manager:  # Открытие контекстного менеджера
+            manager.cursor.execute("""SELECT name_company, name_vacancy, salary_from, salary_to, alternate_url
+                                        FROM vacancy
+                                        INNER JOIN company ON company.id_company = vacancy.id_employer""")
+            # Выполнение запроса
+            return manager.cursor.fetchall()  # Возврат результата запроса
+
+    def get_companies_and_vacancies_count(self): # TODO: Проерить sql запрос и как-то протестировать
+        """Метод, который получает список всех компаний и количество вакансий в каждой из них."""
+        with self.manager as manager: # Открытие контекстного менеджера
+            manager.cursor.execute("""SELECT name_company, COUNT(name_vacancy) FROM company
+                                     INNER JOIN vacancy ON company.id_company = vacancy.id_employer
+                                     GROUP BY name_company""") # Выполнение запроса
+            return manager.cursor.fetchall()  # Возврат результата запроса
+
+    def get_avg_salary(self): # TODO: Проерить sql запрос и как-то протестировать
+        """Метод, который получает среднюю зарплату по всем вакансиям."""
+        with self.manager as manager:  # Открытие контекстного менеджера
+            manager.cursor.execute("""SELECT AVG(salary_from) + AVG(salary_to) /2 AS avg_salary
+                                        FROM vacancy""")  # Выполнение запроса
+            return manager.cursor.fetchall()  # Возврат результата запроса
+
+    def get_vacancies_with_higher_salary(self): # TODO: Проерить sql запрос и как-то протестировать
+        """Метод, который получает список всех вакансий, у которых зарплата выше средней по всем вакансиям."""
+        with self.manager as manager: # Открытие контекстного менеджера
+            manager.cursor.execute("""SELECT name_vacancy
+                                        FROM vacancy
+                                        WHERE (salary_from + salary_to) / 2 >
+                                        (SELECT (AVG(salary_from) + AVG(salary_to)) / 2 AS salary_avg 
+                                        FROM vacancy)""") # Выполнение запроса
+            return manager.cursor.fetchall() # Возврат результата запроса
+
+    def get_vacancies_with_keyword(self, keyword):  # TODO: Проерить sql запрос и как-то протестировать
+        """Метод, который получает список всех вакансий, в названии которых содержатся переданные в метод слова"""
+        with self.manager as manager: # Открытие контекстного менеджера
+            manager.cursor.execute("""SELECT name_vacancy
+                                    FROM vacancy
+                                    WHERE name_vacancy LIKE %s""", (f"%{keyword}%",)) # Выполнение запроса,
+            # передача параметра в запрос в виде кортежа (f"%{keyword}%",)
+            return manager.cursor.fetchall()    # Возврат результата запроса
 
 
-service = Service()
-db_manager = DBManager()
+service = Service()  # Создание объекта класса Service. Для того чтобы использовать методы класса Service
+db_manager = DBManager() # Создание объекта класса DBManager. Для того чтобы использовать методы класса DBManager
 
-service.manager = db_manager
-service.get_vacancies()
+service.manager = db_manager  # Установка значения атрибута manager объекта service. Для того чтобы использовать
+# методы класса DBManager
+service.get_aLL_vacancies()  # Вызов метода get_aLL_vacancies объекта service.
+service.get_companies_and_vacancies_count()  # Вызов метода get_companies_and_vacancies_count объекта service
+service.get_avg_salary() # Вызов метода get_avg_salary объекта service
+service.get_vacancies_with_higher_salary()  # Вызов метода get_vacancies_with_higher_salary объекта service
+service.get_vacancies_with_keyword('Моряк')  # Вызов метода get_vacancies_with_keyword объекта service
+
