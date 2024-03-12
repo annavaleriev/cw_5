818f449a6a044bfabfd7aa0e1e21c74d28d494a4
annavaleriev <filenko.a@gmail.com>
2024-03-12 17:26:33 +0300

    Изменила sql запросы

--- db.sql
+++ db.sql
@@ -1,16 +1,16 @@
 CREATE TABLE vacancy
 (
+	id_vacancy serial PRIMARY key, 
 	name_vacancy varchar NOT NULL,
 	salary_from integer,
 	salary_to integer,
 	salary_currency varchar,
 	alternate_url varchar NOT NULL,
-	id_employer integer NOT NULL
+	id_employer integer references company(id_company)
 );
 
 CREATE TABLE company
 (
-	id_company integer PRIMARY KEY,
-	name_company varchar NOT NULL,
-	url_vacancies varchar NOT NULL
+	id_company serial  PRIMARY KEY,
+	name_company varchar NOT NULL
 );
